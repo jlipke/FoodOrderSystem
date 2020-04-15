28e3f1b14c169124a3e30f69128b46485616acdf
William Lewandowski <700230112@fvtc.edu>
2020-04-15 17:43:02 -0500

    Created the BL, BL.Models, BL.Test and DB projects

---
+++ FoodOrderSystem/FoodOrderSystem.BL.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FoodOrderSystem.BL.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FoodOrderSystem.BL.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4a20ed31-9e78-48e1-81a8-5dfc97a78347")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
