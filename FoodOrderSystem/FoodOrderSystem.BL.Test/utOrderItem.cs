8c6baff233959b1da6458ec0d214f04c695dfb41
J Lipke <l.jlipke@gmail.com>
2020-04-26 16:28:51 -0500

    utOrderItem Completed

--- FoodOrderSystem/FoodOrderSystem.BL.Test/utOrderItem.cs
+++ FoodOrderSystem/FoodOrderSystem.BL.Test/utOrderItem.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FoodOrderSystem.BL;
+using FoodOrderSystem.BL.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace FoodOrderSystem.BL.Test
 {
@@ -7,8 +11,40 @@ namespace FoodOrderSystem.BL.Test
     public class utOrderItem
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RunAll()
         {
+            LoadTest();
+            InsertTest();
         }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            List<OrderItem> orderItem = OrderItemManager.Load();
+
+            int expected = 1;
+            int actual = orderItem.Count;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            Guid orderId = OrderManager.Load().FirstOrDefault().Id;
+            Guid menuItemId = MenuItemManager.Load().FirstOrDefault().Id;
+
+            OrderItem orderItem = new OrderItem
+            {
+                Id = Guid.NewGuid(),
+                OrderId = orderId,
+                MenuItemId = menuItemId
+            };
+
+            bool result = OrderItemManager.Insert(orderItem);
+            Assert.IsTrue(result);
+
+        }
+        
     }
 }
