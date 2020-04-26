e055095bed635e938d47ca42609727b21a6ed5ae
J Lipke <l.jlipke@gmail.com>
2020-04-26 14:12:55 -0500

    OrderItemManager completed

--- FoodOrderSystem/FoodOrderSystem.BL/OrderItemManager.cs
+++ FoodOrderSystem/FoodOrderSystem.BL/OrderItemManager.cs
@@ -3,11 +3,220 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FoodOrderSystem.BL.Models;
+using FoodOrderSystem.PL;
 
 namespace FoodOrderSystem.BL
 {
-    public class OrderItemManager
+    public static class OrderItemManager
     {
+        public static List<OrderItem> Load()
+        {
+            try
+            {
+                using (FoodOrderSystemEntities dc = new FoodOrderSystemEntities())
+                {
+                    List<OrderItem> results = new List<OrderItem>();
+                    dc.tblOrderItems.ToList().ForEach(p => results.Add(new OrderItem
+                    {
+                        Id = p.Id,
+                        OrderId = p.OrderId,
+                        MenuItemId = p.MenuItemId,
+
+                    }));
+                    
+                    return results;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public static OrderItem LoadById(Guid id)
+        {
+            try
+            {
+                using (FoodOrderSystemEntities dc = new FoodOrderSystemEntities())
+                { 
+                    List<OrderItem> results = new List<OrderItem>();
+                    tblOrderItem orderItem = dc.tblOrderItems.FirstOrDefault(m => m.Id == id); // Get the id that matches the one that was passed in
+
+                    dc.tblOrderItems.ToList().ForEach(v => results.Add(new OrderItem
+                    {
+                        Id = v.Id,
+                        OrderId = v.OrderId,
+                        MenuItemId = v.tblMenuItem.Id
+                    }));
+                    
+
+                    if (results != null)
+                    {
+                        return new OrderItem
+                        {
+                            Id = orderItem.Id,
+                            OrderId = orderItem.OrderId,
+                            MenuItemId = orderItem.tblMenuItem.Id
+                            
+                        };
+                    }
+                    else
+                    {
+                        throw new Exception("Cannot find the row.");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public static bool Insert(OrderItem orderItem)
+        {
+            try
+            {
+                using (FoodOrderSystemEntities dc = new FoodOrderSystemEntities())
+                {
+                    // Make a new row
+                    tblOrderItem newrow = new tblOrderItem();
+
+                    // Set the properties
+                    newrow.Id = Guid.NewGuid();
+                    newrow.OrderId = orderItem.OrderId;
+                    newrow.MenuItemId = orderItem.MenuItemId;
+
+                    // Do the Insert
+                    dc.tblOrderItems.Add(newrow);
+
+                    // Commit the insert
+                    dc.SaveChanges();
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public static bool Insert(Guid orderId, Guid menuItemId)
+        {
+            try
+            {
+                using (FoodOrderSystemEntities dc = new FoodOrderSystemEntities())
+                {
+                    // Make a new row
+                    tblOrderItem newrow = new tblOrderItem();
+
+                    // Set the properties
+                    newrow.Id = Guid.NewGuid();
+                    newrow.OrderId = orderId;
+                    newrow.MenuItemId = menuItemId;
+
+                    // Do the Insert
+                    dc.tblOrderItems.Add(newrow);
+
+                    // Commit the insert
+                    dc.SaveChanges();
+
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public static int Update(Guid id, Guid orderId, Guid menuItemId)
+        {
+            try
+            {
+                using (FoodOrderSystemEntities dc = new FoodOrderSystemEntities())
+                {
+                    tblOrderItem updatedrow = dc.tblOrderItems.FirstOrDefault(a => a.Id == id);
+
+                    if (updatedrow != null)
+                    {
+                        updatedrow.OrderId = orderId;
+                        updatedrow.MenuItemId = menuItemId;
+
+
+                        return dc.SaveChanges();
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static int Update(OrderItem orderItem)
+        {
+            try
+            {
+                using (FoodOrderSystemEntities dc = new FoodOrderSystemEntities())
+                {
+                    tblOrderItem updatedrow = dc.tblOrderItems.FirstOrDefault(a => a.Id == orderItem.Id);
+
+                    if (updatedrow != null)
+                    {
+                        updatedrow.OrderId = orderItem.OrderId;
+                        updatedrow.MenuItemId = orderItem.MenuItemId;
+
+
+                        return dc.SaveChanges();
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static int Delete(Guid id)
+        {
+            try
+            {
+                using (FoodOrderSystemEntities dc = new FoodOrderSystemEntities())
+                {
+                    tblOrderItem deletedrow = dc.tblOrderItems.FirstOrDefault(a => a.Id == id);
+
+                    if (deletedrow != null)
+                    {
+                        dc.tblOrderItems.Remove(deletedrow);
+
+                        return dc.SaveChanges();
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
 
     }
 }
