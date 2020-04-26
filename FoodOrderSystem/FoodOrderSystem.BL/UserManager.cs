8b26cd3a03079d8b8dea4492a620fd8b3b936a5d
J Lipke <l.jlipke@gmail.com>
2020-04-25 22:35:44 -0500

    Started UserManager

--- FoodOrderSystem/FoodOrderSystem.BL/UserManager.cs
+++ FoodOrderSystem/FoodOrderSystem.BL/UserManager.cs
@@ -3,11 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FoodOrderSystem.BL.Models;
+using FoodOrderSystem.PL;
 
 namespace FoodOrderSystem.BL
 {
-    public class UserManager
+    public static class UserManager
     {
+        public static List<User> Load()
+        {
+            try
+            {
+                using (FoodOrderSystemEntities dc = new FoodOrderSystemEntities())
+                {
+                    List<User> results = new List<User>();
+                    dc.tblUsers.ToList().ForEach(p => results.Add(new User
+                    {
+                        Id = p.Id,
+                        FirstName = p.FirstName,
+                        LastName = p.LastName,
+                        Email = p.Email,
+                        Phone = p.Phone,
+                        Password = p.Password,
+                    }));
 
+                    return results;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        public static bool Insert()
+        {
+            try
+            {
+                using (FoodOrderSystemEntities dc = new FoodOrderSystemEntities())
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
     }
 }
