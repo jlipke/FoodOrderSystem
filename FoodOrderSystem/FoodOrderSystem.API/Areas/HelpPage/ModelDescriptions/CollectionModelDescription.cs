252f9ca2db254f2794e3d767535690ef6d7ed0e0
J Lipke <l.jlipke@gmail.com>
2020-04-26 17:35:57 -0500

    Initial Creation of API

---
+++ FoodOrderSystem/FoodOrderSystem.API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace FoodOrderSystem.API.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ FoodOrderSystem/FoodOrderSystem.API/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FoodOrderSystem.API.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
