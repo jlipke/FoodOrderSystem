61c80003a652b0ecb5a26b703e21a63d54fd6ece
J Lipke <l.jlipke@gmail.com>
2020-04-26 16:34:42 -0500

    utOrder Complete

--- FoodOrderSystem/FoodOrderSystem.BL.Test/utOrder.cs
+++ FoodOrderSystem/FoodOrderSystem.BL.Test/utOrder.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FoodOrderSystem.BL;
+using FoodOrderSystem.BL.Models;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace FoodOrderSystem.BL.Test
 {
@@ -7,8 +11,44 @@ namespace FoodOrderSystem.BL.Test
     public class utOrder
     {
         [TestMethod]
-        public void TestMethod1()
+        public void RunAll()
         {
+            LoadTest();
+            InsertTest();
         }
+
+        [TestMethod]
+        public void LoadTest()
+        {
+            List<Order> order = OrderManager.Load();
+
+            int expected = 1;
+            int actual = order.Count;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InsertTest()
+        {
+            Guid userId = UserManager.Load().FirstOrDefault().Id;
+            Guid addressId = UserAddressManager.Load().FirstOrDefault().Id;
+            Guid paymentId = UserPaymentManager.Load().FirstOrDefault().Id;
+            DateTime date = DateTime.Now;
+
+            Order order = new Order
+            {
+                UserId = userId,
+                AddressId = addressId,
+                PaymentId = paymentId,
+                Date = date
+            };
+
+            bool result = OrderManager.Insert(order);
+            Assert.IsTrue(result);
+
+        }
+
+        
     }
 }
