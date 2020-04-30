2a0c14f0abd991b629fe2e491dee37007dc1acbe
J Lipke <l.jlipke@gmail.com>
2020-04-29 19:34:00 -0500

    Added Xamarin Project

---
+++ FoodOrderSystem/FoodOrderSystem.Xamarin/FoodOrderSystem.Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FoodOrderSystem.Xamarin.Models;
+
+namespace FoodOrderSystem.Xamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
