0ece8b7d29bc742834f1dfffd209c6157f580c88
J Lipke <l.jlipke@gmail.com>
2020-05-06 23:36:38 -0500

    Android still not connecting to api project.

---
+++ FoodOrderSystem/FoodOrderSystem.Xamarin/FoodOrderSystem.Xamarin/Views/MenuItemsPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using FoodOrderSystem.Xamarin.Models;
+using FoodOrderSystem.Xamarin.Views;
+using FoodOrderSystem.Xamarin.ViewModels;
+
+namespace FoodOrderSystem.Xamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuItemsPage : ContentPage
+    {
+        MenuItemsViewModel viewModel;
+
+        public MenuItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new MenuItemsViewModel();
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var item = args.SelectedItem as MenuItem;
+            if (item == null)
+                return;
+
+            await Navigation.PushAsync(new MenuItemDetailPage(new MenuItemDetailViewModel(item)));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.MenuItems.Count == 0)
+                viewModel.LoadMenuItemsCommand.Execute(null);
+        }
+    }
+}
